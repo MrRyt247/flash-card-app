91755fd5ef5198902e9464c20c886476921ad01b
Mr. Ryt <flaviosobbin04@gmail.com>
2025-08-19 19:46:52 +0000

    :sparkles: creates card component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,30 @@
 <script lang="ts">
+	import Card from './components/Card.svelte';
 	import Progressbar from './components/Progressbar.svelte';
 	import { data } from './components/data';
 
 	let total: number = data.length;
 	let index: number = $state(0);
+	let currentQuestion: { question: string; answer: string } = data[index];
+	let isAnsShown: boolean = $state(false);
 </script>
 
 <h1>Flash Cards</h1>
 
 <Progressbar {index} {total} />
+<div class="container">
+	<Card data={currentQuestion} />
+</div>
+
+<style>
+	.container {
+		background-color: #fff;
+		border: solid 3px lightgrey;
+		border-radius: 10px;
+		padding: 3px;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		position: relative;
+	}
+</style>
---
+++ src/routes/components/Card.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    let { data } = $props();
+</script>
+
+<div class="card">
+   <h2>{ data.question }</h2>
+</div>
+
+<style>
+.card {
+    border-radius: 5px;
+    background-color: #eee;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    font-size: 18px;
+    color: #333;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    transition: transform 0.2s ease-in-out;
+}
+</style>
